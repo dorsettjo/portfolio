0052b7781cf729574bf79e9ba3c1cb9d0bd9d055
Josie Dorsett <josie@MacBook-Pro-3.local>
2017-02-12 15:02:34 -0500

    after removal of columns

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@
 jQuery(document).ready(function($) {
 	$('.toggle-menu').jPushMenu();
 });
+
+function crisisList(){
+        $.getJSON("/api/crisiss/").done(function(results){
+            var source = $("#handlebarsTest").html();
+            var template = Handlebars.compile(source);
+            var html = template(results.results);
+            $('#crisisResults').append(html);
+        })
+}
+
+
+crisisList()
